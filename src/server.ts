3d8884b46e4c050a54eb5f4aede70b3079719462
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2024-10-04 07:03:53 -0300

    04/10

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { mainRouter } from "./routers/main";
 
 const server = express();
 server.use(helmet());
@@ -9,6 +10,7 @@ server.use(urlencoded({ extended: true }));
 server.use(express.json());
 
 // ROTAS
+server.use(mainRouter)
 
 server.listen(process.env.HTTP_PORT || 3000, () => {
     console.log(`Servidor rodarndo em ${process.env.BASE_URL}`);
