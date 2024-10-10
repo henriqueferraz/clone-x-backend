6fe121d42ba325205edb7f9ad3d98a8cd99d8e3c
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2024-10-10 19:09:32 -0300

    10/10

--- src/routers/main.ts
+++ src/routers/main.ts
@@ -4,3 +4,15 @@ import * as pingController from "../controllers/ping"
 export const mainRouter = Router();
 
 mainRouter.get('/ping', pingController.ping);
+//mainRouter.get('/privateping');
+
+mainRouter.post('/auth/singnup');
+mainRouter.post('/auth/singnin');
+
+mainRouter.post('/tweet'); // Inserir um tweet
+mainRouter.get('/tweet/:id'); // Pegar um tweet especifico
+mainRouter.get('/tweet/:id/answers'); // Respostas do tweet
+mainRouter.post('/tweet/:id/like'); // Dar ou retirar um like no tweet
+
+mainRouter.get('/user/:slug'); //Slug do usuário
+mainRouter.get('/user/:slug/tweets'); //Tweets do usuario
