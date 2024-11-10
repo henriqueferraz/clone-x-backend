8987e3ad05e84fc031253fb00f37a481595d392c
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2024-11-10 08:12:03 -0300

    10/11

--- src/controllers/tweet.ts
+++ src/controllers/tweet.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { ExtendedRequest } from "../types/extended-request";
 import { addTweetSchema } from "../schemas/add-tweet";
 import { createTweet, findTweet } from "../services/tweet";
+import { addHashtag } from "../services/trend";
 
 export const addTweet = async (req: ExtendedRequest, res: Response) => {
     // validar os dados enviados
@@ -24,7 +25,16 @@ export const addTweet = async (req: ExtendedRequest, res: Response) => {
         safeData.data.body,
         safeData.data.answer ? parseInt(safeData.data.answer) : 0
     )
+
     // adicionar a Hashtag ao trend
+    const hashtags = safeData.data.body.match(/#[a-zA-Z0-9_]+/g);
+    if (hashtags) {
+        for (let hashtag of hashtags) {
+            if (hashtag.length >= 2) {
+                await addHashtag(hashtag);
+            }
+        }
+    }
 
     res.json({ tweet: newTweet })
 }
